4fb72bcf5e0199551b4df0da802b53f93eed87e5
ideceddy <edwingmundo2@gmail.com>
2016-03-02 07:20:49 -0500

    Learning

---
+++ hello.php
@@ -0,0 +1,8 @@
+<html>
+	<head>
+		<title> <?php echo "Hello Git-Hub"; ?> </title>
+	</head>
+	<body>
+		<h2> <?php echo "Hello Git-Hub I'm working on learning how to use Git"; ?> </h2>
+	</body>
+</html>
