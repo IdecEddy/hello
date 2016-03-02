a0035f85886582fffd00f8aa606d95542d8d3e10
ideceddy <edwingmundo2@gmail.com>
2016-03-02 07:43:35 -0500

    just doing some test

--- hello.php
+++ hello.php
@@ -1,6 +1,6 @@
 <html>
 	<head>
-		<title> <?php echo "Hello Git-Hub"; ?> </title>
+		<title> <?php echo "Hello Git-Hub"; //this is a test I just want to push some crap?> </title>
 	</head>
 	<body>
 		<h2> <?php echo "Hello Git-Hub I'm working on learning how to use Git"; ?> </h2>
